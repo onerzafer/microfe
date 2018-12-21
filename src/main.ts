ae4e17da9c83b2d07d60ce4d00c7d9dd0bd707d3
Öner Zafer <Oener.Zafer@de.atu.eu>
2018-12-21 14:58:34 +0100

    added static file serving support

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useStaticAssets(join(__dirname, 'micro-app-registry'));
   await app.listen(3000);
 }
 bootstrap();
