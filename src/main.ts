588a1de129381e4e524bd938ddd1ac9ba06c7c15
Öner Zafer <Oener.Zafer@de.atu.eu>
2018-12-27 09:28:19 +0100

    styling

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,8 @@ import { AppModule } from './app.module';
 import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useStaticAssets(join(__dirname, 'micro-app-registry'));
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule);
+    app.useStaticAssets(join(__dirname, 'micro-app-registry'));
+    await app.listen(3000);
 }
 bootstrap();
